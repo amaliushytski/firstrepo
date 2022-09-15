ee2679b0ba2efbccfcb338162af12ac00b113d21
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-09-15 21:06:25 +0200

    Add files via upload

---
+++ First.java
@@ -0,0 +1,61 @@
+package by.tr.main;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("M");
+		System.out.println("\t" + "A");
+		System.out.println("L");
+		System.out.println("\t" + "I");
+		System.out.println("U");
+		System.out.println("\t" + "S");
+		System.out.println("H");
+		System.out.println("\t" + "Y");
+		System.out.println("T");
+		System.out.println("\t" + "S");
+		System.out.println("K");
+		System.out.println("\t" + "I");
+
+		System.out.println("Next task------------------------------");
+
+		System.out.println("******************");
+		System.out.println("*" + "\t" + "*" + "\t" + "*");
+		System.out.println("");
+		System.out.println("*" + "\t" + "*" + "\t" + "*");
+		System.out.println("");
+		System.out.println("*" + "\t" + "*" + "\t" + "*");
+		System.out.println("");
+		System.out.println("*" + "\t" + "*" + "\t" + "*");
+		System.out.println("******************");
+
+		System.out.println("Next task------------------------------");
+
+		int x = 10;
+		int y = 5;
+
+		int sum = x + y;
+		int sub = x - y;
+		int mult = x * y;
+		int div = x / y;
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(mult);
+		System.out.println(div);
+
+		System.out.println("Next task------------------------------");
+
+		int a = 4;
+
+		int c = 3 + a;
+		System.out.println(c);
+		System.out.println("Next task------------------------------");
+
+		int z = 2 * x + (y - 2) * 5;
+		System.out.println(z);
+		System.out.println("Next task------------------------------");
+
+		int b = 4;
+		int z1 = ((a - 3) * b / 2) + c;
+		System.out.println(z1);
+	}
+}
